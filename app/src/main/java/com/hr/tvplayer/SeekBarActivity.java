5d54c2c7c59839fa713bfc653b80bbdca27949ba
lv <1107748200@qq.com>
2018-05-22 11:40:12 +0800

    更新下

--- app/src/main/java/com/hr/tvplayer/SeekBarActivity.java
+++ app/src/main/java/com/hr/tvplayer/SeekBarActivity.java
@@ -2,10 +2,13 @@ package com.hr.tvplayer;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 /**
  * Created by 吕 on 2018/5/21.
@@ -13,6 +16,7 @@ import android.view.WindowManager;
 
 public class SeekBarActivity extends Activity{
     WindowManager windowManager;
+    WindowManager.LayoutParams params;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +25,15 @@ public class SeekBarActivity extends Activity{
         setContentView(R.layout.activity_seek);
 
         windowManager = (WindowManager)this.getSystemService(Context.WINDOW_SERVICE);
-
+        params = new WindowManager.LayoutParams();
+        params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.width = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.format = PixelFormat.TRANSLUCENT;
+        params.type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL;
+        params.flags =
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        params.gravity = Gravity.TOP | Gravity.LEFT;
 
     }
 
