6d82403a873ca3fcc670e0d57a2e0fd1ab73581b
lv <1107748200@qq.com>
2018-05-22 16:14:18 +0800

    更新下

--- app/src/main/java/com/hr/tvplayer/SeekBarActivity.java
+++ app/src/main/java/com/hr/tvplayer/SeekBarActivity.java
@@ -6,9 +6,11 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 /**
  * Created by 吕 on 2018/5/21.
@@ -17,6 +19,7 @@ import android.widget.FrameLayout;
 public class SeekBarActivity extends Activity{
     WindowManager windowManager;
     WindowManager.LayoutParams params;
+    private LinearLayout   linearLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,21 +27,40 @@ public class SeekBarActivity extends Activity{
 
         setContentView(R.layout.activity_seek);
 
+        linearLayout = (LinearLayout) LayoutInflater.from(getApplication()).inflate(R.layout.view_audio_surfaceview, null);
+
+
         windowManager = (WindowManager)this.getSystemService(Context.WINDOW_SERVICE);
         params = new WindowManager.LayoutParams();
-        params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
-        params.width = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.height = 200;
+        params.width = 400;
         params.format = PixelFormat.TRANSLUCENT;
         params.type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL;
         params.flags =
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                         | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.gravity = Gravity.CENTER;
 
     }
 
     public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_show:
+
+
+                windowManager.addView(linearLayout, params);
+
+                break;
+            case R.id.btn_change:
+
+                params.gravity = Gravity.BOTTOM;
+                windowManager.updateViewLayout(linearLayout,params);
+
+                break;
+        }
 
 
     }
+
+
 }
